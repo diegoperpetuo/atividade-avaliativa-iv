743043c6ecead9d0575def620d7eabd27e611a0c
Diego Perpétuo <104659648+diegoperpetuo@users.noreply.github.com>
2025-05-10 02:37:47 +0000

    Fix route

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 connectDatabase();
 
-app.use('/api', authRoutes);
+app.use('/src', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
